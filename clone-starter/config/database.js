2198fb3fb6d63c6263e05858dca1ee6146c4f028
Laura-Alford <89553183+Laura-Alford@users.noreply.github.com>
2021-11-08 11:22:01 -0500

    Add files via upload

---
+++ clone-starter/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://la:LATest@cluster0.q2ojb.mongodb.net/clone-starter?retryWrites=true&w=majority', 
+    'dbName': 'clone-starter'
+};
